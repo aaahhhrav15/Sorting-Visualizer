94152695e1675538529ec20414d4cfc437da442d
Aarav <lodhaaarav12005@gmail.com>
2024-02-17 01:20:09 +0530

    first commit

---
+++ sorting-visualizer/src/components/Homepage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import "../assets/homepage.css";
+import Navbar from './Navbar';
+
+const Homepage = () => {
+  return (
+    <Navbar/>
+  )
+};
+
+export default Homepage;
---
+++ sorting-visualizer/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "../assets/navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+        <div className="logo">
+            <h2>Sorting Visualizer</h2>
+        </div>
+    </div>
+  )
+};
+
+export default Navbar;
