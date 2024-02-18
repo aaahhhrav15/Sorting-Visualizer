f5f28a8119d00e240801b2adb57ac74c1dc94e13
Aarav <lodhaaarav12005@gmail.com>
2024-02-19 00:38:39 +0530

    created boxes depending on user inout

--- sorting-visualizer/src/components/Selection.jsx
+++ sorting-visualizer/src/components/Selection.jsx
@@ -3,31 +3,55 @@ import "../assets/selection.css";
 
 const Selection = () => {
   
-  const [size, setSize]=useState();
+  const [size, setSize] = useState();
+  const [elements, setElements] = useState();
+  const [arr,setArr] = useState([]);
 
-  const handleChange = (event)=>{
+  const sizeChange = (event)=>{
     setSize(event.target.value);
   }
+  
+  const elementChange = (event)=>{
+    setElements(event.target.value);
+  }
 
-  const handleSubmit = (event)=>{
+  const getValues = (event)=>{
     event.preventDefault();
     console.log(size);
+    console.log(elements);
+    if(elements)
+    {
+      const newArr = elements.split(",").map(Number);
+      setArr(newArr);
+      console.log(arr);
+    }
   }
+  
+  
 
   return (
     <div className="selection">
       <div className="features">
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={getValues}>
           <div className="sizeInput">
             <label htmlFor="size">Array Size <span>(2 - 20)</span></label>
-            <input type="text" name="size" id="size" onChange={handleChange} />
+            <input type="text" name="size" id="size" onChange={sizeChange} />
           </div>
           <div className="elements">
             <label htmlFor="">Array Elements</label>
-            <input type="text" name="elements" id="elements"/>
+            <input type="text" name="elements" id="elements" onChange={elementChange}/>
           </div>
           <input type="submit" value="Sort"/>
         </form>
+      </div>
+      <div className="selectionCode">
+        <div className="boxes">
+          {
+            arr.map((value,index)=>{
+              return <div key={index} className="box">{value}</div>
+            })
+          }
+        </div>
       </div>
     </div>
   )
