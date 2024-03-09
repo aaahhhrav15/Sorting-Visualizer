587dcd5b5dc233ed29a277c9e78875c7c6159d09
Aarav <lodhaaarav12005@gmail.com>
2024-03-10 05:22:43 +0530

    fixed the broken code

--- sorting-visualizer/src/components/Selection.jsx
+++ sorting-visualizer/src/components/Selection.jsx
@@ -1,28 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import "../assets/selection.css";
 
 const Selection = () => {
   
   const [size, setSize] = useState();
   const [elements, setElements] = useState();
-  const [arr,setArr] = useState([]);
-  const [color,setColor]=useState(); 
-  const [minIndexColor,setMinIndexColor]=useState();
+  const [arr, setArr] = useState([]);
+  const [color, setColor] = useState(); 
+  const [minIndexColor, setMinIndexColor] = useState();
+  const [distance, setDistance] = useState(0);
 
 
-  const sizeChange = (event)=>{
+  const sizeChange = (event) => {
     setSize(event.target.value);
   }
-  
-  const elementChange = (event)=>{
+  const elementChange = (event) => {
     setElements(event.target.value);
   }
 
-  const getValues = (event)=>{
+  const getValues = (event) => {
     event.preventDefault();
-    console.log(size);
-    console.log(elements);
-    if(elements)
+    if(elements) 
     {
       const newArr = elements.split(",").map(Number);
       setArr(newArr);  
@@ -30,32 +28,54 @@ const Selection = () => {
     }
   }
 
-  const selectionSort = async(array)=>{
-    const arrLength=array.length;
-    const newArr=[...array];
-    for(let i=0;i<arrLength-1;i++)
+  const selectionSort = async(array) => {
+    const arrLength = array.length;
+    const newArr = [...array];
+    for(let i = 0; i < arrLength - 1; i++) 
     {
-      let minIndex=i;
-      for(let j=i+1;j<arrLength;j++)
+      let minIndex = i;
+      for(let j = i + 1; j < arrLength; j++) 
       {
         setColor([minIndex, j]);
         await new Promise(resolve => setTimeout(resolve, 800));
-        if(newArr[j]<newArr[minIndex])
+        if(newArr[j] < newArr[minIndex]) 
         {
-          minIndex=j;
+          minIndex = j;
         }
         setColor([]);
       }
       setMinIndexColor(minIndex);
       await new Promise(resolve => setTimeout(resolve, 800));
-      if(minIndex !== i)
+      if(minIndex !== i) 
       {
-        let temp=newArr[i];
-        newArr[i]=newArr[minIndex];
-        newArr[minIndex]=temp;
+        
+        let temp = newArr[i];
+        newArr[i] = newArr[minIndex];
+        newArr[minIndex] = temp;
+        setArr([...newArr]); 
+        await new Promise(resolve => setTimeout(resolve, 800));
       }      
     }
+    setColor([]);
+    setMinIndexColor();
   }
+
+  useEffect(() => {
+    if (arr.length > 0 && minIndexColor !== undefined && minIndexColor < arr.length - 1) 
+    {
+      const div1 = document.getElementById(`div-${minIndexColor}`);
+      const div2 = document.getElementById(`div-${minIndexColor + 1}`);
+  
+      if (div1 && div2) 
+      {
+        const rect1 = div1.getBoundingClientRect();
+        const rect2 = div2.getBoundingClientRect();
+  
+        const distanceBetweenDivs = rect2.left - rect1.right;
+        setDistance(distanceBetweenDivs);
+      }
+    }
+  }, [arr, minIndexColor]);
   
 
   return (
@@ -75,24 +95,20 @@ const Selection = () => {
       </div>
       <div className="selectionCode">
         <div className="boxes">
-          {
-            arr.map((value,index)=>{
+          {arr.map((value, index) => {
               let bgColor = '';
-              if (color[0] === index || color[1] === index) 
-              {
+              if (color[0] === index || color[1] === index) {
                 bgColor = "green";
-              } 
-              else if (index === minIndexColor) 
-              {
+              } else if (index === minIndexColor) {
                 bgColor = "red";
               }
-              return <div key={index} className="box" style={{ backgroundColor: bgColor }}>{value}</div>
-            })
-          }
+              return (
+                <div key={index} className="box" id={`div-${index}`} style={{ backgroundColor: bgColor }}>{value}</div>
+              );
+            })}
         </div>
       </div>
     </div>
-  )
+  );
 }
-
 export default Selection;
